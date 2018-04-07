eb5f152d0f925854e6ca549d33d78eba4ed36b79
soykolibri <clarice.robenalt@gmail.com>
2018-04-06 17:36:40 -0700

    updated location_history so that it uses a proper window function. added fl_empty_groups, fl_level_at_service, and post_history

--- models/location_history.sql
+++ models/location_history.sql
@@ -1,5 +1,9 @@
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
-select dumpster_id, arrived_at, is_active
+select dumpster_id,
+location_id,
+arrived_at,
+lead(arrived_at) over w as "departed_at"
 from platform.dumpster_arrival
 where is_active
+window w as (partition by dumpster_id order by arrived_at asc)
---
+++ target/compiled/compology/fl_level_at_service.sql
@@ -0,0 +1,39 @@
+
+
+with empty_groups as (
+  select *
+  from "dbt_clarice"."fl_empty_groups"
+  where empty_group_end is not null
+), no_camera_blocked as (
+  select dumpster_id, org_id, post_timestamp, agg_level
+  from "dbt_clarice"."post_history"
+  where container_type = 'front load'
+  and agg_level is not null
+), posts_with_empty_groups as (
+  select ncb.*,
+  lag(ncb.agg_level) over (partition by ncb.dumpster_id, eg.empty_group_id order by ncb.post_timestamp asc) as "lag_level",
+  eg.empty_group_id
+  from no_camera_blocked ncb
+  left join empty_groups eg
+  on (ncb.dumpster_id = eg.dumpster_id
+    and ncb.post_timestamp > eg.empty_group_start
+    and ncb.post_timestamp <= eg.empty_group_end)
+  where eg.empty_group_end is not null
+), max_level as (
+  select distinct on (dumpster_id, empty_group_id) dumpster_id,
+  empty_group_id,
+  max(agg_level) over (partition by dumpster_id, empty_group_id) as "max_level"
+  from posts_with_empty_groups
+  order by dumpster_id, empty_group_id
+)
+select distinct on (peg.dumpster_id, peg.empty_group_id) peg.dumpster_id,
+peg.org_id,
+peg.empty_group_id,
+peg.post_timestamp,
+peg.agg_level as "level_at_service",
+peg.lag_level as "level_prior_to_service",
+ml.max_level as "max_level_before_service"
+from posts_with_empty_groups peg
+left join max_level ml
+on (peg.dumpster_id = ml.dumpster_id and peg.empty_group_id = ml.empty_group_id)
+order by peg.dumpster_id, peg.empty_group_id, peg.post_timestamp desc
